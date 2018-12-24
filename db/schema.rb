2648d02187aeabc0985103f33a969e0d6cb462ad
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-12-24 21:56:27 +0900

    create_items-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_122829) do
+ActiveRecord::Schema.define(version: 2018_12_24_124833) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -24,6 +24,27 @@ ActiveRecord::Schema.define(version: 2018_12_24_122829) do
     t.integer "parent_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "discription", null: false
+    t.string "gender"
+    t.integer "price", null: false
+    t.string "material"
+    t.string "origin"
+    t.string "delivery_days", null: false
+    t.integer "wrapping", null: false
+    t.bigint "shop_id"
+    t.bigint "brand_id"
+    t.bigint "parent_category_id"
+    t.bigint "child_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_items_on_brand_id"
+    t.index ["child_category_id"], name: "index_items_on_child_category_id"
+    t.index ["parent_category_id"], name: "index_items_on_parent_category_id"
+    t.index ["shop_id"], name: "index_items_on_shop_id"
   end
 
   create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -44,4 +65,8 @@ ActiveRecord::Schema.define(version: 2018_12_24_122829) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "brands"
+  add_foreign_key "items", "categories", column: "child_category_id"
+  add_foreign_key "items", "categories", column: "parent_category_id"
+  add_foreign_key "items", "shops"
 end

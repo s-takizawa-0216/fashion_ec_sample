e9dad0f15c6150b9b5b3f197a67679d16e71b908
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-12-27 00:32:37 +0900

    implement_dot_pics

--- app/assets/javascripts/item_show.js
+++ app/assets/javascripts/item_show.js
@@ -4,8 +4,14 @@ $(function() {
     appendArrows: $('.prevNextCtrl'),
     prevArrow: '<li class="fa fa-angle-left" id="btnPrev"></div>',
     nextArrow: '<li class="fa fa-angle-right" id="btnNext"></div>',
-    dots: true,
-    appendDots: $('#photoThimb'),
-    speed: 300,
+    speed: 0,
   });
 });
+
+$(function() {
+  $('#photoThimb img').click(function(){
+    var $thisImg = $(this).attr('src');
+    $('#photoMain img').attr({src: $thisImg})
+  });
+});
+

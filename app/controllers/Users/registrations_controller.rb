8eb67ac13357cd3f3669777b127e3a920c1e532c
RYO KAINUMA <kr49kr21@gmail.com>
2018-12-26 13:25:20 +0900

    Create done_signup view
    Create done_sighup view

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  def done_signUp
+  def done_signup
     
   end
   # GET /resource/sign_up
@@ -55,6 +55,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for(resource)
     super(resource)
+    user_resistration_done_signup
   end
 
   # The path used after sign up for inactive accounts.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'users/done_signup',  to: 'users/registrations#done_signup', as: :user_resistration_done_signup
+  end
+
   root "items#index"
 end
 

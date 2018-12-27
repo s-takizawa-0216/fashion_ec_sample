19afe1efe127981eeafa9c9f9defee17e0872a74
RYO KAINUMA <kr49kr21@gmail.com>
2018-12-27 21:55:02 +0900

    Set image_uploadr
    Set image_uploadr to post items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,24 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @item.images.build
+    @brand = Brand.all
+    @shop = Shop.all
+    @parent_category = Category.where(depth: 0)
+    @child_category = Category.where(depth: 1)
+
   end
 
   def create
+    @item = Item.new(create_params)
+    @item.save
+      redirect_to new_item_path
   end
 
   private
 
   def create_params
+    params.require(:item).permit(:name, :discription, :gender, :price, :material, :origin, :delivery_days, :wrapping, :shop_id, :brand_id, :parent_category_id, :child_category_id, images_attributes: [:image])
   end
 
 end

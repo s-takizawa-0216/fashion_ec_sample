68ae38e976a6dac7e4ffb646ea99c5a7baf800bb
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-12-27 18:56:34 +0900

    create_slide_change_pics

--- app/assets/javascripts/item_show.js
+++ app/assets/javascripts/item_show.js
@@ -3,6 +3,8 @@ $(function(){
     var photoList = $('#photoGallery').find("#photoMain li");
     var thimb = [];
     var current = 0;
+    var btnPrev = $('#btnPrev')
+    var btnNext = $('#btnNext')
 
     function open(){
       $(photoList[current]).stop().fadeIn(10)
@@ -30,8 +32,58 @@ $(function(){
     };
     init()
     open()
+
+    function open1(){
+      btnPrev.off('click');
+      btnNext.off('click');
+      $(photoList[current]).stop().fadeIn(0, checkControl);
+    };
+
+    function close1(){
+      $(photoList).stop().fadeOut(0)
+    }
+
+    function clickControl(type){
+      close1();
+      switch(type){
+        case 'btnPrev':
+          current--;
+          break;
+        case 'btnNext':
+          current++;
+          break;
+      };
+      open1();
+    }
+
+    function checkControl(){
+      switch(current){
+        case 0:
+          hideControl(btnPrev);
+          showControl(btnNext);
+          break;
+        case photoList.length-1:
+          showControl(btnPrev);
+          hideControl(btnNext);
+          break;
+        default:
+          showControl(btnPrev);
+          showControl(btnNext);
+          break;
+      };
+    };
+
+    function hideControl(btn){
+      btn.hide();
+      btn.off('click');
+    };
+
+    function showControl(btn){
+      btn.show();
+      btn.off('click').on('click', function(){clickControl($(btn).attr('id'));});
+    };
+
+    checkControl();
   };
-
-
   photoChange('#photoGallery')
 });

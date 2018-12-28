d3fa357c43b13a694edc27a6ecee55459dd504fb
shun <s.takizawa.0216@gmail.com>
2018-12-28 15:22:21 +0900

    create menu.js
    メニュー部分のアコーディオンのjs

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -30,7 +30,7 @@ $(function(){
     })
   });
 
-  $('.header__right__popup__inner__comment').mouseleave(function(){
+  $('.header__right__popup__inner-comment').mouseleave(function(){
     $('.header__right__notice-js').css({
       'background-color': '#2d2d2d'
     })
@@ -45,3 +45,78 @@ $(function(){
    });
 
 });
+
+$(function(){
+
+  $('.header__right__fav-js').mouseover(function(){
+    $('.header__right__fav-js').css({
+      'background-color': 'black'
+    })
+  });
+
+  $('.header__right__popup__inner-fav').mouseleave(function(){
+    $('.header__right__fav-js').css({
+      'background-color': '#2d2d2d'
+    })
+  });
+
+  $('.header__right__fav-js').mouseover(function(){
+    $('.header__right__popup__inner-fav').show();
+  });
+
+   $('.header__right__popup__inner-fav').mouseleave(function(){
+    $('.header__right__popup__inner-fav').hide();
+   });
+
+});
+
+
+$(function(){
+
+  $('.header__right__cart-js').mouseover(function(){
+    $('.header__right__cart-js').css({
+      'background-color': 'black'
+    })
+  });
+
+  $('.header__right__popup__inner-cart').mouseleave(function(){
+    $('.header__right__cart-js').css({
+      'background-color': '#2d2d2d'
+    })
+  });
+
+  $('.header__right__cart-js').mouseover(function(){
+    $('.header__right__popup__inner-cart').show();
+  });
+
+   $('.header__right__popup__inner-cart').mouseleave(function(){
+    $('.header__right__popup__inner-cart').hide();
+   });
+
+});
+
+$(function(){
+
+  $('.header__right__menu-js').mouseover(function(){
+    $('.header__right__menu-js').css({
+      'background-color': 'black'
+    })
+  });
+
+  $('.header__right__popup__inner-list').mouseleave(function(){
+    $('.header__right__menu-js').css({
+      'background-color': '#2d2d2d'
+    })
+  });
+
+  $('.header__right__menu-js').mouseover(function(){
+    $('.header__right__popup__inner-list').show();
+  });
+
+   $('.header__right__popup__inner-list').mouseleave(function(){
+    $('.header__right__popup__inner-list').hide();
+   });
+
+});
+
+

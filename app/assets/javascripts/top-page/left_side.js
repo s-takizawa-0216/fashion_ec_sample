2586354b935eb2b13089a1fa9b19deed64be2fad
shun <s.takizawa.0216@gmail.com>
2018-12-29 23:26:17 +0900

    create left_side.js
    left_sideのアコーディオン部分のjs

---
+++ app/assets/javascripts/top-page/left_side.js
@@ -0,0 +1,47 @@
+$(function(){
+
+  $('#category-js-show').mouseover(function(){
+    $('#category-js-hide').css('left',155)
+  })
+
+  $('.left__content__list-category').mouseout(function(){
+    $('#category-js-hide').css('left', 999999)
+  })
+
+
+  $('#category-js-show-outer').mouseover(function(){
+    $('#category-js-hide-outer').css('left',155)
+  })
+
+  $('.left__content__list-category').mouseout(function(){
+    $('#category-js-hide-outer').css('left', 999999)
+  })
+
+   $('#category-js-show-pant').mouseover(function(){
+    $('#category-js-hide-pant').css('left',155)
+  })
+
+  $('.left__content__list-category').mouseout(function(){
+    $('#category-js-hide-pant').css('left', 999999)
+  })
+
+   $('#fav-js-show').mouseover(function(){
+    $('#fav-js-hide').css('left',155)
+  })
+
+  $('.left__content__list-category').mouseout(function(){
+    $('#fav-js-hide').css('left', 999999)
+  })
+
+  $('#fav-js-show-shop').mouseover(function(){
+    $('#fav-js-hide-shop').css('left',155)
+  })
+
+  $('.left__content__list-category').mouseout(function(){
+    $('#fav-js-hide-shop').css('left', 999999)
+  })
+
+});
+
+
+

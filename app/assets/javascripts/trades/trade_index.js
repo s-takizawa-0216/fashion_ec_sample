bf1e0de3a281b9bbeaba7ea4350321a8011ca0c7
RYO KAINUMA <kr49kr21@gmail.com>
2019-01-01 01:47:59 +0900

    Create item-count
    Create item-count

--- app/assets/javascripts/trades/trade_index.js
+++ app/assets/javascripts/trades/trade_index.js
@@ -1,3 +1,4 @@
+// カート画面の購入確認ボタン
 $(function(){
   $('button#register-btn').click(function(){
     $('.cart-main__middle__right__footer__usual').hide();
@@ -5,6 +6,7 @@ $(function(){
   });
 });
 
+// カート画面の　購入確認のタブの切り替え
 $(function(){
   $('#facebooktab').click(function(){
     $('.cart-main__middle__right-tab-zozo').hide();
@@ -23,8 +25,23 @@ $(function(){
   });
 });
 
+// カート画面の商品個数カウント 
 $(function(){
-  $('#minus').click(function(){
-   
+  var step = 1;
+  var min = 1;
+
+  var count = $("[id=number]").val();
+  var count = parseInt(count);
+
+  $('[id=minus]').click(function(){
+    count -= step
+  if( count < min ) { count = min; };
+    $("[id=number]").val(count);
+  });
+
+  $('[id=plus]').click(function(){
+    count += step
+    $("[id=number]").val(count);
   });
 });
+

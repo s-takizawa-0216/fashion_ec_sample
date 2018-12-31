66d6d4d4babdd0c2b83ba8ba63b0691a33b44969
RYO KAINUMA <kr49kr21@gmail.com>
2018-12-31 17:24:51 +0900

    Set tab zooid and facebook
    Set tab zooid and facebook to shift the view of purchase

---
+++ app/assets/javascripts/trades/trade_index.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('button#register-btn').click(function(){
+    $('.cart-main__middle__right__footer__usual').hide();
+    $('.cart-main__middle__right__footer__open').slideToggle();
+  });
+});
+
+$(finction(){
+
+});

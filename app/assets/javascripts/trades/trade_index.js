48347cb9c4a98fb8a1980e7677a8d1b859749290
RYO KAINUMA <kr49kr21@gmail.com>
2018-12-31 22:39:30 +0900

    Edit trades/index.html.haml
    Edit trades/index.html.haml

--- app/assets/javascripts/trades/trade_index.js
+++ app/assets/javascripts/trades/trade_index.js
@@ -5,6 +5,26 @@ $(function(){
   });
 });
 
-$(finction(){
+$(function(){
+  $('#facebooktab').click(function(){
+    $('.cart-main__middle__right-tab-zozo').hide();
+    $('.cart-main__middle__right__facebookbox').css('display', 'block');
+    $('#zozotab').removeClass('selected');
+    $(this).addClass('selected');
+  });
+});
 
+$(function(){
+  $('#zozotab').click(function(){
+    $('.cart-main__middle__right__facebookbox').hide();
+    $('.cart-main__middle__right-tab-zozo').css('display', 'block');
+    $('#facebooktab').removeClass('selected');
+    $(this).addClass('selected');
+  });
+});
+
+$(function(){
+  $('#minus').click(function(){
+   
+  });
 });

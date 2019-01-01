3687c6253e229635b10b5f5e518eb92fb450049e
RYO KAINUMA <kr49kr21@gmail.com>
2019-01-01 18:07:41 +0900

    Create query to make  a selection of delivery ways
    Create query to make  a selection of delivery ways

--- app/assets/javascripts/trades/order.js
+++ app/assets/javascripts/trades/order.js
@@ -1,14 +1,47 @@
+// 住所追加項目
 $(function(){
-  $('#add-address').click(function(){
-    $('#vanish-address').hide();
-    $('#new-address').slideToggle();
+  $('#new-address-check').click(function(){
+    $('#new-address').show();
+    $('#address-header').removeClass('unchecked');
+    $('#address-header').addClass('checked');
   });
 });
 
 $(function(){
   $('#hide-new-address').click(function(){
     $('#new-address').hide();
-    $('#vanish-address').show();
-
+    $('#address-header').addClass('unchecked');
+    
   });
 });
+
+// 発送方法選択項目
+$(function(){
+  $('#new-delivery-check').click(function(){
+    $('#new-delivery').show();
+    $('#delivery-header').removeClass('unchecked');
+    $('#delivery-header').addClass('checked');
+    $('#date-new-delivery').hide();
+    $('#date-delivery-header').addClass('unchecked');
+  });
+});
+
+$(function(){
+  $('#hide-new-delivery').click(function(){
+    $('#new-delivery').hide();
+    $('#delivery-header').addClass('unchecked');
+    $('#date-new-delivery').hide();
+    $('#date-delivery-header').addClass('unchecked');
+  });
+});
+
+$(function(){
+  $('#date-new-delivery-check').click(function(){
+    $('#date-new-delivery').show();
+    $('#date-delivery-header').removeClass('unchecked');
+    $('#date-delivery-header').addClass('checked');
+    $('#new-delivery').hide();
+    $('#delivery-header').addClass('unchecked');
+  });
+});
+

5683ce6706f4903cb5bd88b3aca5210fa09315c7
shun <s.takizawa.0216@gmail.com>
2019-01-04 14:24:02 +0900

    edit coordination.js

--- app/assets/javascripts/top-page/coordination.js
+++ app/assets/javascripts/top-page/coordination.js
@@ -4,13 +4,13 @@ $(function(){
   $('#show__rank1').mouseover(function(){
     $('#show__rank1-js').addClass('showup')
 
-    $('#show__rank1-js').css('top',2697)
+    $('#show__rank1-js').css('top',-126)
   })
 
   $('  .right__content__coordination__show').mouseleave(function(){
     $('#show__rank1-js').removeClass('showup')
 
-    $('#show__rank1-js').css('top',2840)
+    $('#show__rank1-js').css('top',42)
   })
 
 
@@ -18,61 +18,60 @@ $(function(){
   $('#show__rank2').mouseover(function(){
     $('#show__rank2-js').addClass('showup')
 
-    $('#show__rank2-js').css('top',2697)
+    $('#show__rank2-js').css('top',-126)
   })
 
   $('  .right__content__coordination__show').mouseleave(function(){
     $('#show__rank2-js').removeClass('showup')
 
-    $('#show__rank2-js').css('top',2840)
+    $('#show__rank2-js').css('top',42)
   })
 
   $('#show__rank3').mouseover(function(){
     $('#show__rank3-js').addClass('showup')
 
-    $('#show__rank3-js').css('top',2697)
+    $('#show__rank3-js').css('top',-126)
   })
 
   $('  .right__content__coordination__show').mouseleave(function(){
     $('#show__rank3-js').removeClass('showup')
-
-    $('#show__rank3-js').css('top',2840)
+    $('#show__rank3-js').css('top',42)
   })
 
   $('#show__rank4').mouseover(function(){
     $('#show__rank4-js').addClass('showup')
 
-    $('#show__rank4-js').css('top',3028)
+    $('#show__rank4-js').css('top',-126)
   })
 
   $('  .right__content__coordination__show').mouseleave(function(){
     $('#show__rank4-js').removeClass('showup')
 
-    $('#show__rank4-js').css('top',3240)
+    $('#show__rank4-js').css('top',42)
   })
 
   $('#show__rank5').mouseover(function(){
     $('#show__rank5-js').addClass('showup')
 
-    $('#show__rank5-js').css('top',3028)
+    $('#show__rank5-js').css('top',-126)
   })
 
   $('  .right__content__coordination__show').mouseleave(function(){
     $('#show__rank5-js').removeClass('showup')
 
-    $('#show__rank5-js').css('top',3240)
+    $('#show__rank5-js').css('top',42)
   })
 
   $('#show__rank6').mouseover(function(){
     $('#show__rank6-js').addClass('showup')
 
-    $('#show__rank6-js').css('top',3028)
+    $('#show__rank6-js').css('top',-126)
   })
 
   $('  .right__content__coordination__show').mouseleave(function(){
     $('#show__rank6-js').removeClass('showup')
 
-    $('#show__rank6-js').css('top',3240)
+    $('#show__rank6-js').css('top',42)
   })
 
 

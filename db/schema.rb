11261e9c290f0c0f773e1f0e05f0237fd21ac9a9
KEITA TAIRA <kt10580@hotmail.co.jp>
2019-01-09 21:05:26 +0900

    first_comit

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2019_01_05_110038) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "colors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false

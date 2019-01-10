3eec98bec456b0611944854dec05f2bc024187fd
shun <s.takizawa.0216@gmail.com>
2019-01-10 16:39:53 +0900

    create cordination_left-bar.js
    身長・価格部分のスライダーのjs

---
+++ app/assets/javascripts/cordination/cordination-left-bar.js
@@ -0,0 +1,40 @@
+$(function(){
+  $('#slider').slider({
+    min: 50,
+    max: 200,
+    step: 2,
+    range: true,
+    // 2初期値（配列指定）
+    values: [0, 200],
+    // 3スライダー変更時／初期化時の処理
+    change: function(e, ui) {
+      $('#min').val(ui.values[0]);
+      $('#max').val(ui.values[1]);
+    },
+    create: function(e, ui) {
+      var values = $(this).slider('option', 'values')
+      $('#min').val(values[0]);
+      $('#max').val(values[1]);
+    }
+  });
+
+  $('#slider-price').slider({
+    min: 50,
+    max: 200,
+    step: 2,
+    range: true,
+    // 2初期値（配列指定）
+    values: [0, 200],
+    // 3スライダー変更時／初期化時の処理
+    change: function(e, ui) {
+      $('#min-price').val(ui.values[0]);
+      $('#max-price').val(ui.values[1]);
+    },
+    create: function(e, ui) {
+      var values = $(this).slider('option', 'values')
+      $('#min-price').val(values[0]);
+      $('#max-price').val(values[1]);
+    }
+  });
+})
+

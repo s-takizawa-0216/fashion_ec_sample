c591bff79c3e52030fdf2ff81a1221ea7a9bb2bb
RYO KAINUMA <kr49kr21@gmail.com>
2019-01-11 02:13:21 +0900

    Add delete button
    Add delete button

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -4,24 +4,27 @@ class TradesController < ApplicationController
   def index
     @open_trade = Trade.where(status: 0, user_id: current_user.id)
     @once_in_cart = Trade.where(status: 1, user_id: current_user.id)
+    @items_sum = @open_trade.sum{|trade|trade[:total]}
+    @sum_arigato = @items_sum*0.9
   end
 
   def minus_count
-    @trade = Trade.find(params[:trade_id])
-    count_items = @trade.update(count: @trade.count-1)
-    total_price = @trade.update(total: @trade.stock.item.price*@trade.count)
+    trade = Trade.find(params[:trade_id])
+    count_items = trade.update(count: trade.count-1)
+    total_price = trade.update(total: trade.stock.item.price*trade.count)
     redirect_to trades_path
   end
 
   def plus_count
-    @trade = Trade.find(params[:trade_id])
-    count_items = @trade.update(count: @trade.count+1)
-    total_price = @trade.update(total: @trade.stock.item.price*@trade.count)
+    trade = Trade.find(params[:trade_id])
+    count_items = trade.update(count: trade.count+1)
+    total_price = trade.update(total: trade.stock.item.price*trade.count)
     redirect_to trades_path
   end
 
   def destroy_item
-    @trade = Trade.find(params[:trade_id])
+    trade = Trade.find(params[:trade_id])
+    trade.destroy
     redirect_to trades_path
   end
 

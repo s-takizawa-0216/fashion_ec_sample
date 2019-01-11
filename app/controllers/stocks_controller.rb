a581ad7bfa1ca3b5c5b99c3a647075f82623bcd7
KEITA TAIRA <kt10580@hotmail.co.jp>
2019-01-11 20:44:22 +0900

    create_funcion_of_stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,22 @@
 class StocksController < ApplicationController
   def index
+    @stock = Stock.new
     shop = Shop.find(1)
     @items = shop.items
+  end
+
+  def create
+    @stock = Stock.new(stock_params)
+    if @stock.save
+      redirect_to stocks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(:item_id, :color_id, :size_id, :count)
   end
 end
--- app/models/color.rb
+++ app/models/color.rb
@@ -1,3 +1,3 @@
 class Color < ApplicationRecord
-  has_many :stock
+  has_many :stocks
 end

2ebd7762773c29c888b26e5c1f025301587aed1f
RYO KAINUMA <kr49kr21@gmail.com>
2019-01-12 17:40:35 +0900

    Delete query.jpostl
    I am sorry

--- app/assets/javascripts/trades/order.js
+++ app/assets/javascripts/trades/order.js
@@ -104,15 +104,3 @@ $(function(){
     }
   });
 });
-
-//郵便番号での住所検索
-$ ->
-  $("#user_postcode").jpostal({
-    postcode : [ "#user_postcode" ],
-    address  : {
-                  "#user_prefecture_code" : "%3",
-                  "#user_address_city"            : "%4",
-                  "#user_address_street"          : "%5%6%7"
-                }
-  });
-

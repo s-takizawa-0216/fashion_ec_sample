6086a6a56682b6b086b9890e4fc3dab532a4ece4
KEITA TAIRA <kt10580@hotmail.co.jp>
2019-01-12 16:16:48 +0900

    change_case_noImage

--- app/assets/javascripts/stack_index.js
+++ app/assets/javascripts/stack_index.js
@@ -16,22 +16,27 @@ $(function(){
 
 })
 
+
 $(function(){
   var modal = $(".imagePreview-overlay");
+  var mainImage = $(".stockDetailPix img")
+  var noImage = modal.find(".noImage img").attr("src");
 
   modal.hide()
 
-  $(".modal-close").on("click", function(){
-    modal.hide();
+  $("#tblStockInfo .iconIMG").on("click", function(){
+    var image = $(this).find("img").attr("src");
+
+    mainImage.attr("src", noImage);
+
+    if( image != ""){
+      mainImage.attr("src", image);
+    }
+    modal.show();
   })
 
-
-  $("#tblStockInfo .iconIMG").on("click", function(){
-
-    var image = $(this).find("img").attr("src");
-    $(".stockDetailPix img").attr("src", image);
-
-    modal.show();
+  $(".modal-close").on("click", function(){
+    modal.hide();
   })
 
 })

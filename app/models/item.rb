dd4297cbeccee91933bcbb3d21bb174a75f6bcc5
shun <s.takizawa.0216@gmail.com>
2019-01-13 17:25:18 +0900

    edit brand.rb item.rb
    brand.rb はアソシエーションの追記
    item.rb はランキング機能よう

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
 
+  is_impressionable counter_cache: true
+
   has_many :images
+  belongs_to :brand
   accepts_nested_attributes_for :images
   validates :name, :discription, :price, :delivery_days, :wrapping, presence: true
 

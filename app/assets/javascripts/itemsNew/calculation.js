36dfbf0ec9b0a49b08bbc49bbe914b70e9e953b6
RYO KAINUMA <kr49kr21@gmail.com>
2019-01-15 05:33:19 +0900

    Edit calcuratuin

--- app/assets/javascripts/itemsNew/calculation.js
+++ app/assets/javascripts/itemsNew/calculation.js
@@ -1,14 +1,30 @@
 // クーポン適用価格の表示
 $(function () {
-  $('#item_coupon, #item_price' ).change(function() {
+  $('#item_coupon').change(function() {
 
     $("input#calc").next().remove();
 
-    var coupon = $("item_coupon").val();
+    var coupon = $(this).val();
     var price = $("#item_price").val();
     var calc = coupon*price
-    console.log(calc);
 
+    if(calc > 0){
     $("input#calc").val(calc);
+    }
+  });
+});
+
+$(function () {
+  $('#item_price').change(function() {
+
+    $("input#calc").next().remove();
+
+    var coupon = $(this).val();
+    var price = $("#item_coupon").val();
+    var calc = coupon*price
+
+    if(calc > 0){
+    $("input#calc").val(calc);
+    }
   });
 });

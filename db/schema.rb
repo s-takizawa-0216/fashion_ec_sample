99cff3f7a22d3d09182159ba87792e8d2789069c
shun <s.takizawa.0216@gmail.com>
2019-01-19 20:42:22 +0900

    edit schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_01_18_102421) do
-
-
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false

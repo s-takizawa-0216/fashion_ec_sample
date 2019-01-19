31c4c062b7ddda59475a5edbdbefad76ed367bef
shun <s.takizawa.0216@gmail.com>
2019-01-19 13:54:33 +0900

    edit stock.rb user.rb

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,8 +3,10 @@ class Stock < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   has_many :trades
+  has_many :fav_stocks
   belongs_to :item
   belongs_to :color
   belongs_to :size
+
 
 end

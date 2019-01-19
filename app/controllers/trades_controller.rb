11517524aa1a0e8e9cd3d46fac262418232f2129
shun <s.takizawa.0216@gmail.com>
2019-01-19 14:03:39 +0900

    edit trades controller

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -111,6 +111,17 @@ class TradesController < ApplicationController
     done_transaction = trade.update(status: 2)
   end
 
+
+def create
+    trade = Trade.new(trade_params)
+    if trade.save
+      redirect_to trades_path
+    else
+      render "items/show"
+    end
+  end
+
+
   private
 
     def credit_card_params
@@ -120,5 +131,8 @@ class TradesController < ApplicationController
     def shipping_info_params
       params.require(:shipping).permit(:genre, :name, :postal_code, :prefecture, :address1, :address2, :phonenumber).merge(user_id: current_user.id)
     end
-end
 
+    def trade_params
+      params.require(:trade).permit(:stock_id, :total).merge(status: 0, user_id: current_user.id, count: 1)
+    end
+end

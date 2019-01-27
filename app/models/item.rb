120ddb7ea9aca412cf7f47711dccd800f29c9684
shun <s.takizawa.0216@gmail.com>
2019-01-27 14:16:06 +0900

    commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
 
-  is_impressionable counter_cache: true
+  # is_impressionable counter_cache: true
   belongs_to :shop
   belongs_to :parent_category, class_name: 'Category', :foreign_key => 'parent_category_id'
   belongs_to :child_category, class_name: 'Category', :foreign_key => 'child_category_id'

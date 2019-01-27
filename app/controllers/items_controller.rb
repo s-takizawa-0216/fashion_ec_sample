721945f6e0d88d08d34eba294a2e9b85989255de
shun <s.takizawa.0216@gmail.com>
2019-01-27 14:30:55 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,13 +8,13 @@ class ItemsController < ApplicationController
     @zozo = Item.order("created_at DESC").limit(3)
     @coupon_item = Item.where.not(coupon: 1).order("created_at DESC").limit(3)
     @coupon_other_item = Item.where.not(coupon: 1).order("created_at DESC").offset(3).take(4)
-    # @rank1 = Item.order('impressions_count DESC').take(1)
-    # @rank2 = Item.order('impressions_count DESC').offset(1).take(1)
-    # @rank3 = Item.order('impressions_count DESC').offset(2).take(1)
+    @rank1 = Item.order('impressions_count DESC').take(1)
+    @rank2 = Item.order('impressions_count DESC').offset(1).take(1)
+    @rank3 = Item.order('impressions_count DESC').offset(2).take(1)
     @number1 = [*1..3].take(1)
     @number2 = [*2..3].take(1)
     @number3 = [*3..3].take(1)
-    # @other_most_viewd = Item.order('impressions_count DESC').offset(3).take(20)
+    @other_most_viewd = Item.order('impressions_count DESC').offset(3).take(20)
     @number4_23 = [*4..23]
 
     @brand_rank1 = Brand.offset(0).take(1)
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
     @shop_other_rank = Shop.offset(3).take(17)
     @shop_other_number_4_20 = [*4..20]
 
-    # @rank1_shop = Shop.order('impressions_count DESC').take(1)
+    @rank1_shop = Shop.order('impressions_count DESC').take(1)
 
     if user_signed_in?
       @favorite_shops = FavShop.where(user_id:current_user.id)
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,6 +4,6 @@ class Shop < ApplicationRecord
   has_many :stocks, dependent: :destroy
   belongs_to :user
 
-  # is_impressionable counter_cache: true
+  is_impressionable counter_cache: true
 
 end
